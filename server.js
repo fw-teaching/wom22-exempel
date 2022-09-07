1a4f9375bd3b8fbc805c62df55b6b4df9b847cf3
Fredrik Welander <welandfr@arcada.fi>
2022-09-07 15:49:28 +0300

    Lektion 2

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log("Hello Node!!!!")
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3030
+
+const fooRouter = require('./routes/foo.js')
+
+const runAlways = (req, res, next) => {
+    res.locals.myVariable = 'hello from runAlways' // res.locals är ett bra ställe att spara lokala variabler
+    console.log(`A request was made to ${req.path}`)
+    next()
+}
+
+const runSometimes = (req, res, next) => {
+    console.log(`sometimes`)
+    next()
+}
+
+app.use(runAlways)
+
+app.get('/', (req, res) => {
+    res.send(`Express says hello! ${res.locals.myVariable}`)
+})
+
+app.use('/foo', fooRouter)
+
+app.get('/bar', runSometimes, (req, res) => {
+    res.send('bar')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
