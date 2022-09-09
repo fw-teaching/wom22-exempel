ef12742653ce23fcecd0e41f5077af6a407a0575
Fredrik Welander <welandfr@arcada.fi>
2022-09-09 13:38:28 +0300

    Commments

--- routes/foo.js
+++ routes/foo.js
@@ -1,3 +1,4 @@
+// Den här filen är middleware (i det här fallet en route) som en egen fil
 const express = require('express')
 const router = express.Router()
 
@@ -5,5 +6,6 @@ router.get('/', (req, res) => {
     res.send({title: 'foo'})
 })
 
+// Till slut exporterar vi router som en modul så den kan importeras i andra filer
 module.exports = router
 
--- server.js
+++ server.js
@@ -1,32 +1,39 @@
+
+// Vi måste importera express-modulen för att skapa en express-app
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3030
 
+// Vi importerar vor foo-route som en modul
 const fooRouter = require('./routes/foo.js')
 
+
+// Middleware-funktioner
 const runAlways = (req, res, next) => {
     res.locals.myVariable = 'hello from runAlways' // res.locals är ett bra ställe att spara lokala variabler
     console.log(`A request was made to ${req.path}`)
     next()
 }
-
 const runSometimes = (req, res, next) => {
     console.log(`sometimes`)
     next()
 }
 
+// app.use(middleWare) => denna körs för varje request
 app.use(runAlways)
 
 app.get('/', (req, res) => {
     res.send(`Express says hello! ${res.locals.myVariable}`)
 })
 
+// app.use() också för att använda en router-modul
 app.use('/foo', fooRouter)
 
 app.get('/bar', runSometimes, (req, res) => {
     res.send('bar')
 })
 
+// app.listen startar upp vår express-server och lyssnar på requests på en viss port.
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
