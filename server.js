333a77200e9a47aa909ff150558620b649f02bd2
Fredrik Welander <welandfr@arcada.fi>
2022-09-12 15:44:48 +0300

    lektion 3

--- server.js
+++ server.js
@@ -2,7 +2,20 @@
 // Vi måste importera express-modulen för att skapa en express-app
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3030
+
+// Importera dotenv och läs in .env-filen
+require('dotenv').config()
+
+// Använd en variabel ur .env-filen
+console.log(process.env.DOTENV_WORKS)
+
+// skapa och öppna mongoose-connection till MongoDB Atlas
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.on('open', () => console.log('Connected to DB!'))
 
 app.use(express.json())
 
