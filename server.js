91a0f4eace85975ad909b97f61783f4c56dec4f0
Fredrik Welander <welandfr@arcada.fi>
2022-09-12 14:24:36 +0300

    add in-memory Notes app

--- server.js
+++ server.js
@@ -4,36 +4,17 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3030
 
-// Vi importerar vor foo-route som en modul
-const fooRouter = require('./routes/foo.js')
-
-
-// Middleware-funktioner
-const runAlways = (req, res, next) => {
-    res.locals.myVariable = 'hello from runAlways' // res.locals är ett bra ställe att spara lokala variabler
-    console.log(`A request was made to ${req.path}`)
-    next()
-}
-const runSometimes = (req, res, next) => {
-    console.log(`sometimes`)
-    next()
-}
-
-// app.use(middleWare) => denna körs för varje request
-app.use(runAlways)
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send(`Express says hello! ${res.locals.myVariable}`)
+    res.send({ msg: 'Express says hello!'})
 })
 
-// app.use() också för att använda en router-modul
-app.use('/foo', fooRouter)
 
-app.get('/bar', runSometimes, (req, res) => {
-    res.send('bar')
-})
+const notesRouter = require('./routes/notes')
+app.use('/notes', notesRouter)
 
-// app.listen startar upp vår express-server och lyssnar på requests på en viss port.
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
